f3bcae95d397e9275b22d67f36bfd701f80d9139
Michell Hornung <michellhornung@gmail.com>
2019-01-31 19:31:05 -0200

    Adding and deleting row calculating total

--- js/fatura.js
+++ js/fatura.js
@@ -12,23 +12,23 @@ $(document).ready(function () {
 
     //Load with today date  
     document.querySelector("#dataFatura").valueAsDate = new Date();
-    
 
+    /** Masks for fields */
+    $('.money2').mask("#.##0,00", {reverse: true});
+    $('.cnpj').mask('00.000.000/0000-00', {reverse: true});
+    $('.cep').mask('00000-000');
+    //$('#sum').mask("#.##0,00", {reverse: true});
+
+
+    /** ####### */
     /** Dynamically add inputs */
 
     //define template
     var template = $('#sections .section:first').clone();
-
-    //define counter
     var sectionsCount = 1;
-
-    //add new section
     $('body').on('click', '.addsection', function () {
 
-        //increment
         sectionsCount++;
-
-        //loop through each input
         var section = template.clone().find(':input').each(function () {
 
             //set id to store the updated section number
@@ -44,6 +44,7 @@ $(document).ready(function () {
 
             //inject new section
             .appendTo('#sections');
+            cost();
         return false;
     });
 
@@ -53,64 +54,70 @@ $(document).ready(function () {
         $(this).parent().fadeOut(300, function () {
             //remove parent element (main section)
             $(this).parent().parent().remove();
+            calculateSum();
             return false;
         });
         return false;
     });
 
-
-    /** Masks for fields */
-    $('.money2').mask("#.##0,00", {reverse: true});
-    $('.cnpj').mask('00.000.000/0000-00', {reverse: true});
-    $('.cep').mask('00000-000');
-    $('#sum').mask("#.##0,00", {reverse: true});
-
     /** Calculate SUM currency fields */
-    // $(".cost").each(
-    //     function () {
-    //         $(this).keyup(
-    //             function () {
-    //                 calculateSum()
-    //             });
-    //     });
+    $(".cost").each(
+        function () {
+            $(this).keyup(
+                function () {
+                    cost();
+                });
+        });
+
+    function cost() {
+        $(".cost").each(
+            function () {
+                $(this).keyup(
+                    function () {
+                        calculateSum()
+                    });
+            });
+
+    }
+
+    function calculateSum() {
+         var sum = 0;
+        $(".cost").each(
+             function () {
+                 var vl = this.value.replace(',', '');
+                 if (!isNaN(vl) && vl.length != 0) {
+                     sum += parseFloat(vl);
+                 }
+             });
+
+        //$("#sum").val(sum.toFixed(2));
+        $("#sum").val(sum);
+     }
 
 
-    // function calculateSum() {
-    //     var sum = 0;
-    //     $(".cost").each(
-    //         function () {
-    //             var vl = this.value.replace(',', '');
-    //             if (!isNaN(vl) && vl.length != 0) {
-    //                 sum += parseFloat(vl);
-    //             }
-    //         });
+     $('input.cost').keyup(function (event) {
+        // skip for arrow keys
+         if (event.which >= 37 && event.which <= 40) {
+             event.preventDefault();
+         }
+         var $this = $(this);
+         var num = $this.val().replace(/,/gi, "").split("").reverse().join("");
 
-    //     $("#sum").val(sum.toFixed(2));
-    // }
+         var num2 = RemoveRougeChar(num.replace(/(.{3})/g, "$1,").split("").reverse().join(""));
 
-    // $('input.cost').keyup(function (event) {
-    //     // skip for arrow keys
-    //     if (event.which >= 37 && event.which <= 40) {
-    //         event.preventDefault();
-    //     }
-    //     var $this = $(this);
-    //     var num = $this.val().replace(/,/gi, "").split("").reverse().join("");
+         console.log(num2);
 
-    //     var num2 = RemoveRougeChar(num.replace(/(.{3})/g, "$1,").split("").reverse().join(""));
+         // the following line has been simplified. Revision history contains original.
+         $this.val(num2);
+     });
 
-    //     console.log(num2);
+     function RemoveRougeChar(convertString) {
+         if (convertString.substring(0, 1) == ",") {
+             return convertString.substring(1, convertString.length)
+         }
+         return convertString;
 
-    //     // the following line has been simplified. Revision history contains original.
-    //     $this.val(num2);
-    // });
-
-    // function RemoveRougeChar(convertString) {
-    //     if (convertString.substring(0, 1) == ",") {
-    //         return convertString.substring(1, convertString.length)
-    //     }
-    //     return convertString;
-
-    // }
+     }
 
 });
 
