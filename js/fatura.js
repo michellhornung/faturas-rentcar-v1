eec2ec2642da65d59e5f305d7dc7e31bb99e078a
Michell Hornung <michellhornung@gmail.com>
2019-02-01 18:53:39 -0200

    Getting values from inputs to create fatura
    The only to do that was to get a modal into the same page and put values from inputs into the fatura's html. Still needing to end up all the rest, but the first step I did. Will finalize to get all done.

--- js/fatura.js
+++ js/fatura.js
@@ -119,5 +119,129 @@ $(document).ready(function () {
 
      }
 
+     $('#gerarFatura').on('click', function () {
+
+            //GET all values from inputs of form and add into fatura
+            var numeroFatura = $('#numeroFatura').val();
+
+             var faturaContent = ' <div class="container" id="containerFatura" style="border: 1px solid #ccc"> ' +
+             '<h1 class="my-4 text-center">Fatura #' + numeroFatura + '</h1> ' +
+             '<div class="row pad-top-botm "> ' +
+               '<div class="col-lg-6 col-md-6 col-sm-6 text-center"> ' +
+               '<img src="img/logo.jpg" /> ' +
+                 '</div> ' +
+               '<div class="col-lg-6 col-md-6 col-sm-6" style="padding-top: 20px"> ' +
+               '<strong>FOR RENT LOCAÇÃO DE VEÍCULOS.</strong> ' +
+                 '<br /> ' +
+                 '<i>Endereço : Rua Cecilio Toniolo, 50 ' +
+                 '<br /> ' +
+                   'CEP: 80320-160 ' +
+                   '<br /> ' +
+                   'Curitiba - PR - Brasil. </i> ' +
+                   '</div> ' +
+               '</div> ' +
+             '<div class="row text-center contact-info"> ' +
+               '<div class="col-lg-12 col-md-12 col-sm-12"> ' +
+               '<hr /> ' +
+                 '<span> ' +
+                 '<strong>E-mail : </strong> contato@forrentlocacaodeveiculos.com.br ' +
+                   '</span> ' +
+                 '<span> ' +
+                 '<strong>CNPJ : </strong> 31.732.641/0001-64 ' +
+                   '</span> ' +
+                 '<span> ' +
+                 '<strong>Fatura número: </strong> #333777 ' +
+                   '</span> ' +
+                 '<hr /> ' +
+                 '</div> ' +
+               '</div> ' +
+             '<div class="row pad-top-botm client-info"> ' +
+               '<div class="col-lg-6 col-md-6 col-sm-6"> ' +
+               '<h4> <strong>Informações do cliente</strong></h4> ' +
+                 '<strong> John Rambo</strong> ' +
+                 '<br /> ' +
+                 '<b>Endereço :</b> Avenida Brasilia, 4606. ' +
+                 'Curitiba - PR - Brasil. ' +
+                 '<br /> ' +
+                 '<b>CEP: </b> 80320-160 ' +
+                 '<br /> ' +
+                 '<b>CNPJ :</b> 31.732.641/0001-64 ' +
+                 '<br /> ' +
+                 '<b>E-mail :</b> rambo@rambo.com ' +
+                 '</div> ' +
+               '<div class="col-lg-6 col-md-6 col-sm-6"> ' +
+         
+               '<h4> <strong>Detalhes do pagamento </strong></h4> ' +
+                 '<b>Total : R$4.600,00 </b> ' +
+                 '<br /> ' +
+                 'Data : 29/01/2019 ' +
+                 '<br /> ' +
+                 '<b>Status pagamento : PAGO </b> ' +
+                 '</div> ' +
+               '</div> ' +
+             '<div class="row"> ' +
+               '<div class="col-lg-12 col-md-12 col-sm-12"> ' +
+               '<div class="table-responsive"> ' +
+                 '<table class="table table-striped table-bordered table-hover"> ' +
+                   '<thead> ' +
+                     '<tr> ' +
+                       '<th>Descrição do serviço</th> ' +
+                         '<th>Total</th> ' +
+                         '</tr> ' +
+                       '</thead> ' +
+                     '<tbody> ' +
+                     '<tr> ' +
+                       '<td>Pneu pirelli</td> ' +
+                         '<td>400,00</td> ' +
+                         '</tr> ' +
+                       '<tr> ' +
+                       '<td>Roda aro 19 momo</td> ' +
+                         '<td>4000,00</td> ' +
+                         ' </tr> ' + 
+                       '  <tr> ' +
+                       '    <td>Bicos de pneu</td> ' +
+                         '    <td>200,00</td> ' +
+                         '   </tr> ' +
+         
+                       '    </tbody> ' +
+                     '   </table> ' +
+                   '  </div> ' +
+                 '  <hr /> ' +
+                 '  <div class="ttl-amts"> ' +
+                 '<h4> <strong>Total: R$4.600,00</strong> </h4> ' +
+                   '  </div> ' +
+                 ' </div> ' +
+               ' </div> ' +
+             '<div class="row"> ' +
+               '<div class="col-lg-12 col-md-12 col-sm-12"> ' +
+               '<strong> Observações: </strong> ' +
+                 '<ol> ' +
+                 '<li> ' +
+                   'Aqui é o espaço para observações. ' +
+         
+                     '</li> ' +
+                   '<li> ' +
+                   'Nam tristique et ex ut sollicitudin. ' +
+                     'Nunc blandit vel libero id faucibus. Donec posuere justo ornare, malesuada enim eget, auctor lectus. ' +
+         
+                     '</li> ' +
+                   '</ol> ' +
+                 '</div> ' +
+               '</div> ' +
+             '<div class="row pad-top-botm"> ' +
+               '<div class="col-lg-12 col-md-12 col-sm-12 text-right"> ' +
+               '<hr /> ' +
+                 '<button type="button" onclick="callPrinter()" class="btn btn-success">Imprimir Fatura</button> ' +
+                 '<button type="button" class="btn btn-success">Download PDF</button> ' +
+            '   </div> ' +
+            ' </div> ' +
+           '</div> '
+
+            $("#modalBody").append(faturaContent);
+
+    });
+
+
+
 });
 
